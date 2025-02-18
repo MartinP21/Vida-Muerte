eb6cf1af5ba80eb50b7837da004a9ed1e9ee6f3a
MartinP21 <martinpimentelmatos@gmail.com>
2025-02-18 15:09:10 -0400

    -Se agrega una columna al modelo de base de datos.

--- DataAccessLayer/Models/Cita.cs
+++ DataAccessLayer/Models/Cita.cs
@@ -6,24 +6,16 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DataAccessLayer.Models
 {
-    [Index("Cedula", Name = "UQ__Citas__B4ADFE38FCBED69B", IsUnique = true)]
     public partial class Cita
     {
-        [Key]
         public int Id { get; set; }
-        [StringLength(100)]
         public string Nombre { get; set; } = null!;
-        [StringLength(100)]
         public string Apellidos { get; set; } = null!;
-        [StringLength(15)]
-        [Unicode(false)]
         public string Cedula { get; set; } = null!;
-        [StringLength(15)]
-        [Unicode(false)]
         public string Telefono { get; set; } = null!;
-        [Column(TypeName = "datetime")]
         public DateTime FechaCita { get; set; }
         public int IdEstado { get; set; }
+        public string? Motivo { get; set; }
 
         [ForeignKey("IdEstado")]
         [InverseProperty("Cita")]
--- DataAccessLayer/Models/Estado.cs
+++ DataAccessLayer/Models/Estado.cs
@@ -1,12 +1,9 @@
-﻿using System;
+﻿using DataAccessLayer.Models;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using Microsoft.EntityFrameworkCore;
 
-namespace DataAccessLayer.Models
+namespace DataAccessLayer
 {
-    [Table("Estado")]
     public partial class Estado
     {
         public Estado()
@@ -14,14 +11,10 @@ namespace DataAccessLayer.Models
             Cita = new HashSet<Cita>();
         }
 
-        [Key]
         public int IdEstado { get; set; }
-        [StringLength(14)]
         public string? NombreEstado { get; set; }
-        [StringLength(100)]
         public string? Descripcion { get; set; }
 
-        [InverseProperty("IdEstadoNavigation")]
         public virtual ICollection<Cita> Cita { get; set; }
     }
 }

26e0d25f9b62265f5bdb5ee9ed589bff90f42f6b
MartinP21 <martinpimentelmatos@gmail.com>
2025-02-25 15:48:44 -0400

    Se agregan alertas para las diferentes acciones

---
+++ Vida&Muerte/wwwroot/js/alertas.js
@@ -0,0 +1,75 @@
+﻿const btnGuardarEdit = document.querySelector('.btnGuardarEdit');
+const formEdit = document.querySelector(".formEdit");
+const btnCrear = document.querySelector(".btnCrear");
+const formCreate = document.querySelector(".formCreate");
+
+if (btnGuardarEdit){
+    btnGuardarEdit.addEventListener('click', (e) => {
+        e.preventDefault();
+        Swal.fire({
+          title: "Cita editada exitosamente!",
+          icon: "success",
+          draggable: true,
+          confirmButtonText: 'Ok',
+          allowOutsideClick: false
+        }).then((result) => {
+
+            if (result.isConfirmed) {
+                formEdit.submit();
+            }
+        })
+    });
+}
+
+if (btnCrear) {
+    btnCrear.addEventListener('click', (e) => {
+        e.preventDefault();
+        Swal.fire({
+            title: "Cita creada exitosamente!",
+            icon: "success",
+            draggable: true,
+            confirmButtonText: 'Ok',
+            allowOutsideClick: false
+        }).then((result) => {
+
+            if (result.isConfirmed) {
+                formCreate.submit();
+            }
+        })
+    });
+}
+
+const btnDeshabilitar = document.querySelector(".btnDeshabilitar");
+
+if (btnDeshabilitar) {
+    btnDeshabilitar.addEventListener("click", (e) => {
+        mostrarModal();
+
+    })
+}
+
+async function mostrarModal() {
+    debugger
+    const { value: text } = await Swal.fire({
+        input: "textarea",
+        inputLabel: "Motivo para deshabilitar la cita",
+        inputPlaceholder: "Escribir su motivo aqui...",
+        confirmButtonText: 'Guardar',
+        inputAttributes: {
+            "aria-label": "Type your message here"
+        },
+        showCancelButton: true,
+        inputValidator: (value) => {
+            return new Promise((resolve) => {
+                if (value == "") {
+                    resolve("Necesitas escribir un motivo");
+                } else {
+                    resolve();
+                }
+            });
+        }
+    })
+    if (text) {
+        Swal.fire(text);
+    }     
+}
